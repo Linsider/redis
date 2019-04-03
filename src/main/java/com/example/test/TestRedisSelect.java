e60c602bb2afde21ea59ef9c18aa682d73140855
Linsider <32919866+Linsider@users.noreply.github.com>
2019-04-03 15:38:39 +0800

    Update TestRedisSelect.java

--- src/main/java/com/example/test/TestRedisSelect.java
+++ src/main/java/com/example/test/TestRedisSelect.java
@@ -30,4 +30,19 @@ public class TestRedisSelect {
         String two = redisTemplate.opsForValue().get("two");
         return two;
     }
+    
+    /**
+     * 同一个方法中切换不同的redis库
+     * @return
+     */
+    @RequestMapping("/three")
+    @RedisSelect(2)         //选择db2库
+    public String selectThree(){
+        String two = redisTemplate.opsForValue().get("two");
+        System.out.println(two);
+        RedisSelectSupport.select(3);//此处切换到db3库
+        String three = redisTemplate.opsForValue().get("three");
+        System.out.println(three);
+        return three;
+    }
 }
